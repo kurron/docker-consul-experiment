62d9690c6cda7a89c34a06dca777bc08997809ee
Ron Kurr <kurr@kurron.org>
2015-03-19 21:54:58 -0400

    Working command

--- registrator.sh
+++ registrator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --detach --volume /var/run/docker.sock:/tmp/docker.sock --hostname $HOSTNAME gliderlabs/registrator -ip -resync 60 consul://192.168.1.227:8500 
+docker run --detach --volume /var/run/docker.sock:/tmp/docker.sock --hostname $HOSTNAME gliderlabs/registrator -ip 192.168.1.229 consul://192.168.1.229:8500
